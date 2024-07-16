a3d62d13a9e2a28183de8eeaf513ebdf4bf12e7a
anson <alandemy45@gmail.com>
2024-07-16 11:58:52 -0400

    Second commit

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 import os
-
 # Optional: add contact me email functionality (Day 60)
 # import smtplib
 
@@ -28,6 +27,7 @@ pip3 install -r requirements.txt
 
 This will install the packages from the requirements.txt for this project.
 '''
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
@@ -54,12 +54,9 @@ gravatar = Gravatar(app,
                     use_ssl=False,
                     base_url=None)
 
-
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-
-
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
@@ -278,7 +275,6 @@ def about():
 def contact():
     return render_template("contact.html", current_user=current_user)
 
-
 # Optional: You can include the email sending code from Day 60:
 # DON'T put your email and password here directly! The code will be visible when you upload to Github.
 # Use environment variables instead (Day 35)
@@ -288,7 +284,7 @@ def contact():
 
 # @app.route("/contact", methods=["GET", "POST"])
 # def contact():
-#     if request.method  == "POST":
+#     if request.method == "POST":
 #         data = request.form
 #         send_email(data["name"], data["email"], data["phone"], data["message"])
 #         return render_template("contact.html", msg_sent=True)
@@ -304,4 +300,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=False, port=5001)
+    app.run(debug=True, port=5001)
